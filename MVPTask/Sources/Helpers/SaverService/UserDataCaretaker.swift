62d8022f8b75806b5b4267e3eb038a61e5d1df5a
SonataGirl <Ino-sonata1988>
2024-03-15 23:53:32 +0300

    Добавлено сохранение через кейчейн

--- MVPTask/Sources/Helpers/SaverService/UserDataCaretaker.swift
+++ MVPTask/Sources/Helpers/SaverService/UserDataCaretaker.swift
@@ -2,6 +2,7 @@
 // Copyright © RoadMap. All rights reserved.
 
 import Foundation
+import Keychain
 
 /// Класс для сохранения данных пользователя
 final class UserDataCaretaker {
@@ -10,10 +11,15 @@ final class UserDataCaretaker {
     private let encoder = JSONEncoder()
     private let decoder = JSONDecoder()
     private let key = "UserDataProfile"
+    private let passwordKey = "passwordUser"
+    private let loginKey = "loginUser"
+    private let nameKey = "nameUser"
+    private let surnameKey = "surnameUser"
+    private let imageKey = "imageUser"
 
     // MARK: Public Methods
 
-    func save(userProfile: User) {
+    func saveUD(userProfile: User) {
         do {
             let data = try encoder.encode(userProfile)
             UserDefaults.standard.set(data, forKey: key)
@@ -22,7 +28,7 @@ final class UserDataCaretaker {
         }
     }
 
-    func retrieveUserProfileData() -> User? {
+    func retrieveUserProfileDataUD() -> User? {
         guard let data = UserDefaults.standard.data(forKey: key) else {
             return nil
         }
@@ -33,5 +39,32 @@ final class UserDataCaretaker {
             print(error)
             return nil
         }
+    }
+
+    func save(userProfile: User) {
+        Keychain.save(userProfile.password, forKey: passwordKey)
+        Keychain.save(userProfile.login, forKey: loginKey)
+        Keychain.save(userProfile.name, forKey: nameKey)
+        Keychain.save(userProfile.surname, forKey: surnameKey)
+        Keychain.save(userProfile.imageBase64, forKey: imageKey)
+    }
+
+    func retrieveUserProfileData() -> User? {
+        guard let password = Keychain.load(passwordKey),
+              let login = Keychain.load(loginKey),
+              let name = Keychain.load(nameKey),
+              let surname = Keychain.load(surnameKey),
+              let image = Keychain.load(imageKey)
+        else {
+            return nil
+        }
+
+        return User(
+            name: name,
+            surname: surname,
+            imageBase64: image,
+            password: password,
+            login: login
+        )
     }
 }
